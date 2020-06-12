e8965ef6189565e039903006a23b63d1656b900c
lkendre25 <lkendre5652@gmail.com>
2020-06-12 22:50:59 +0530

    Sorting Program

--- ArithOperation.sh
+++ ArithOperation.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-
-
 echo "Arithmatic Operations Here!! "
 
 read -p "Enter the first Number " a
 read -p "Enter the second Number" b
 read -p "Enter the Third Number" c
 
-	exp=$[(a%b)+c]
+	exp4=$[(a%b)+c]
+	exp1=$[a+(b*c)]
+	exp2=$[(a*b)+c]
+	exp3=$[c+(a/b)]
+	ans=($exp4 $exp1 $exp2 $exp3)
 
-	echo "Output of the above expression is : $exp"
+	#echo "Output of the above expression is : $exp"
+	echo "First equestion : ${ans[0]} "
+	echo "Second equation : ${ans[1]} "
+	echo "Third Equation  : ${ans[2]} "
+	echo " Fourth Equaton : ${ans[3]} "
+
+
+	for i in ${!ans[@]}
+	do
+		for j in $i
+		do
+ 			if [ ${ans[$j]} -gt ${ans[1]} ]
+			then
+					temp=${ans[j]}
+					${ans[j]} = ${ans[1]}
+					${ans[1]} = $temp
+
+			fi
+			echo "j : $j "
+			echo "i : $i "
+		done
+
+		echo ${ans[1]}
+	done
+echo ${ans[temp]}
 
