0473f892ba65d17f3b44a66fce37adf275f93331
lkendre25 <lkendre5652@gmail.com>
2020-06-12 20:03:22 +0530

    Commitd

--- ArithOperation.sh
+++ ArithOperation.sh
@@ -7,7 +7,7 @@ read -p "Enter the first Number " a
 read -p "Enter the second Number" b
 read -p "Enter the Third Number" c
 
-	exp=$[c+(a/b)]
+	exp=$[(a%b)+c]
 
 	echo "Output of the above expression is : $exp"
 
